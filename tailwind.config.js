d6392cdaa67eb9db5c00803e1970458775ad12da
Billy Ponto <bpgponto@gmail.com>
2023-02-18 15:29:39 +0800

    feat: setup repo

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		fontFamily: {
+			montserrat: "Montserrat, sans-serif",
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				lg: "75px",
+				xl: "100px",
+			},
+		},
+		extend: {
+			colors: {
+				primary: "#76BC1E",
+				secondary: "#242424",
+				light: "#E9EDF5",
+				lime: "#9BED34",
+				dark: "#1F1F1F",
+        grey: "#959595"
+			},
+		},
+	},
+	plugins: [],
+};
